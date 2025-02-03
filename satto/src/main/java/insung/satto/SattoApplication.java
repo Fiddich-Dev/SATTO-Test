ee1b01c71229728ba0f1d13f3eb5b9d9bd150e7c
insung0927 <78968690+Fiddich-Dev@users.noreply.github.com>
2025-02-04 02:00:12 +0900

    프로젝트 생성

---
+++ satto/src/main/java/insung/satto/SattoApplication.java
@@ -0,0 +1,13 @@
+package insung.satto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SattoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SattoApplication.class, args);
+	}
+
+}
---
+++ satto/src/test/java/insung/satto/SattoApplicationTests.java
@@ -0,0 +1,13 @@
+package insung.satto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SattoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
